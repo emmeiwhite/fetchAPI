ad4b7c68a8c0743862821b40918d52b16525153f
Imran Rafiq Rather <emmeiwhite@gmail.com>
2018-09-26 12:02:19 +0530

    Update promise.js
    Using promise variable- nothing special

--- promise.js
+++ promise.js
@@ -1,8 +1,9 @@
 // I want to fetch the data from the API using fetch('') API: we will use jsonplaceholder
 // The JsonAPI returns the promise
 
-const promise = fetch('https://jsonplaceholder.typicode.com/posts').
-then(function(res){
+const promise = fetch('https://jsonplaceholder.typicode.com/posts');
+
+promise.then(function(res){
     // console.log(res); This here is again a promise, so we have to return it and use another then to get the data
     return res.json(); // We are returning a Promise again which will now be resolved for the data
 }).then(data => {
